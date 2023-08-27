6e05f07ae62578464d615bc7f8f48443a1e839a9
junho.cho <junho.cho@naverlabs.com>
2023-08-27 22:49:22 +0900

    feat: 사용 방법에서 lock 관련 내용 삭제

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="absolute left-1/2 -translate-x-1/2 top-5 p-5 rounded-2xl bg-gray-300 bg-opacity-25">
-	WASD를 통해 이동, 마우스 클릭해 주변 살피기
+	WASD를 통해 이동
 </div>
 <div class="w-screen h-screen">
 	<Canvas useLegacyLights={true}>
